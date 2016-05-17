8e963e87e9b95546bd054db1000e0ba7dbd721e8
Mamta-logiciel <mamta.rani@logicielsolutions.co.in>
2016-05-17 15:42:51 +0530

    Connect Form UI

--- jobprogress.php
+++ jobprogress.php
@@ -9,12 +9,12 @@ Author URI: http://w3guy.com
 */
 
 define( 'JOBPROGRESS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define('API_BASE_URL', 'http://localhost:8000/api/v1');
+define('API_BASE_URL', 'http://api.job_progress.dev/api/v1');
 define('JOBPROGRESS_CLIENT_ID', '123457');
 define('JOBPROGRESS_CLIENT_SECRET', 'schs1EKRpLaS1auhTIc25JrlWSjkry1P');
-define('JOBPRGRESS_AUTHORIZATION_URL', 'http://jobprogress.dev/api/v1/oauth2/authorization');
-define('JOBPRGRESS_DISCONNECT_URL', 'http://jobprogress.dev/api/v1/oauth2/disconnect');
-define('JOBPRGRESS_REFRESH_TOKEN_URL', 'http://localhost:8000/api/v1/oauth2/renew_access_token');
+define('JOBPRGRESS_AUTHORIZATION_URL', 'http://api.job_progress.dev/api/v1/oauth2/authorization');
+define('JOBPRGRESS_DISCONNECT_URL', 'http://api.job_progress.dev/api/v1/oauth2/disconnect');
+define('JOBPRGRESS_REFRESH_TOKEN_URL', 'http://api.job_progress.dev/api/v1/oauth2/renew_access_token');
 define('JOBPRGRESS_REFRESH_TOKEN_GRANT_TYPE', 'refresh_token');
 // run the install scripts upon plugin activation
 // add_action( 'init', array( 'JobProgress', 'init' ) );
