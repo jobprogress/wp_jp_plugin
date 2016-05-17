c03792c71c1210a33637344ea12a8cf2ee2263b8
surjit_logiciel <surjit.singh@logicielsolutions.co.in>
2016-05-17 10:07:52 +0530

    show error message

--- connect-form.php
+++ connect-form.php
@@ -1,6 +1,11 @@
+<?php 
+	if(ine($_GET, 'error_message')) {
+		echo $_GET['error_message'];
+	} 
+?>
 <form method="get" action="<?php echo JOBPRGRESS_AUTHORIZATION_URL ?>">
 <input type="hidden" name="domain" value="<?php echo $domain; ?>">
-<input type="hidden" name="redirect_uri" value="<?php echo $domain.$_SERVER['REQUEST_URI']; ?>">
+<input type="hidden" name="redirect_uri" value="<?php echo $redirect_url; ?>">
 <input type="hidden" name="client_id" value="<?php echo JOBPROGRESS_CLIENT_ID ?>"/>
 <input type="hidden" name="client_secret" value="<?php echo JOBPROGRESS_CLIENT_SECRET ?>"/>
 <input type="hidden" name="grant_type" value="password"/>
