d1a45fedf5476a4c149bab003251d61283f1bd39
surjit_logiciel <surjit.singh@logicielsolutions.co.in>
2016-05-20 10:06:55 +0530

    customer
    jp used instead of jobprogress
    add record limit on customer listing

--- class.customer.php
+++ class.customer.php
@@ -20,8 +20,7 @@ class Customer extends JobProgress {
 	public function __construct() {
 		parent::__construct();
 		add_shortcode( 'jobprogress_customer_form_code', array($this, 'cf_short_code') );
-		add_action( 'admin_menu',array($this, 'jobprogress_admin_page') );
-		// $this->apiData();
+		add_action( 'admin_menu',array($this, 'jp_admin_page') );
 	}
 
 	/**
@@ -40,14 +39,14 @@ class Customer extends JobProgress {
 	 * jobprogress admin section menus show on left side
 	 * @return [type] [description]
 	 */
-	public function jobprogress_admin_page() {
+	public function jp_admin_page() {
 		// show customer label on menu list
 		add_submenu_page( 
-			'jobprogress-admin-page', 
+			'jp-admin-page', 
 			'Customer Manager', 
 			'Customers', 
 			'manage_options', 
-			'customers', 
+			'jp_customers-page', 
 			array($this, 'index')
 		);
 	}
@@ -61,7 +60,7 @@ class Customer extends JobProgress {
 		$page_num = isset( $input['page_num'] ) ? absint( $input['page_num'] ) : 1;
 		$order   = ine($input, 'order') ? $input['order'] : 'desc';
 		$order_by = ine($input, 'order_by') ? $input['order_by'] : 'created_at';
-		$limit = 5; // number of rows in page_num
+		$limit = ine($input, 'limit') ? $input['limit'] : JP_CUSTOMER_LIMIT_PAGINATION; 
 		$offset = ( $page_num - 1 ) * $limit;
 		$total = $this->wpdb->get_var( "SELECT COUNT(id) FROM {$this->wpdb->prefix}customers" );
 		$num_of_pages = ceil( $total / $limit );
@@ -73,7 +72,7 @@ class Customer extends JobProgress {
 		$sql .= " ORDER BY $order_by $order";
 		$sql .= " LIMIT $offset, $limit";
 		$customers = $this->wpdb->get_results( $sql );
-		return require_once( JOBPROGRESS_PLUGIN_DIR . 'customer-index-page.php' );
+		return require_once( JP_PLUGIN_DIR . 'customer-index-page.php' );
 	}
 
 	/**
@@ -82,16 +81,17 @@ class Customer extends JobProgress {
 	 */
 	public function save_customer() {
 		$nonce = $_POST['_wpnonce'];
-		if ( ! wp_verify_nonce( $nonce, 'submit_jobprogress_customer_form' ) ) {
+		if ( ! wp_verify_nonce( $nonce, 'submit_jp_customer_form' ) ) {
 		  return false;
 		}
 		if(isset($_POST) && !empty($_POST)) {
-			require_once( JOBPROGRESS_PLUGIN_DIR . 'class.customer-validator.php' );
+			require_once( JP_PLUGIN_DIR . 'class.customer-validator.php' );
 			$validator = new Customer_Validator;
+		
 			if($validator->is_valid()) {
 				$input = $_POST;
 				$table_name = $this->wpdb->prefix . 'customers';
-				require_once( JOBPROGRESS_PLUGIN_DIR . 'class.customer-data-map.php' );
+				require_once( JP_PLUGIN_DIR . 'class.customer-data-map.php' );
 				$customer =  new Customer_Data_Map($input);
 				$plugin_input = $customer->get_plugin_input();
 				$this->wpdb->insert($table_name, $plugin_input);
@@ -108,22 +108,25 @@ class Customer extends JobProgress {
 	 */
 	public function show_form() {
 		
-		if(($trades = get_transient("jobprogress_trades")) === false) {
-			$trades = $this->get(JOBPRGRESS_TRADE_URL);
-			set_transient("jobprogress_trades", $trades, 86400);
+		if(($trades = get_transient("jp_trades")) === false 
+			|| $trades === '' ) {
+			$trades = $this->get(JP_TRADE_URL);
+			set_transient("jp_trades", $trades, 86400);
 		}
-		$states = $this->get(JOBPRGRESS_STATE_URL);
-		if(($states = get_transient("jobprogress_states")) === false ) {
-			$states = $this->get(JOBPRGRESS_STATE_URL);
-			set_transient("jobprogress_states", $states, 86400);
-		}
-
-		if(($countries = get_transient("jobprogress_countries")) === false) {
-			$countries = $this->get(JOBPRGRESS_COUNTRY_URL);
-			set_transient("jobprogress_countries", $countries, 86400);
+		
+		if(($states = get_transient("jp_states")) === false 
+			|| $states === '') {
+			$states = $this->get(JP_STATE_URL);
+			set_transient("jp_states", $states, 86400);
 		}
 
-		return require_once( JOBPROGRESS_PLUGIN_DIR . 'customer-form-page.php' );
+		if(($countries = get_transient("jp_countries")) === false 
+			|| $countries === '') {
+			$countries = $this->get(JP_COUNTRY_URL);
+			set_transient("jp_countries", $countries, 86400);
+		}
+
+		return require_once( JP_PLUGIN_DIR . 'customer-form-page.php' );
 	}
 
 		
