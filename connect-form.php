daa8dc949f0deca63a09000761bf7fa4b4473987
surjit_logiciel <surjit.singh@logicielsolutions.co.in>
2016-05-20 10:07:40 +0530

    jp used instead of jobprogress

--- connect-form.php
+++ connect-form.php
@@ -3,12 +3,12 @@
 		echo $_GET['error']['message'];
 	}	
 ?>
-<form method="get" action="<?php echo JOBPRGRESS_AUTHORIZATION_URL ?>">
+<form method="get" action="<?php echo JP_AUTHORIZATION_URL ?>">
 <input type="hidden" name="domain" value="<?php echo $domain; ?>">
 <input type="hidden" name="redirect_uri" value="<?php echo $redirect_url; ?>">
-<input type="hidden" name="client_id" value="<?php echo JOBPROGRESS_CLIENT_ID ?>"/>
-<input type="hidden" name="client_secret" value="<?php echo JOBPROGRESS_CLIENT_SECRET ?>"/>
-<?php wp_nonce_field('jobprogress_connect_form'); ?>
+<input type="hidden" name="client_id" value="<?php echo JP_CLIENT_ID ?>"/>
+<input type="hidden" name="client_secret" value="<?php echo JP_CLIENT_SECRET ?>"/>
+<?php wp_nonce_field('jp_connect_form'); ?>
 <input type="hidden" name="grant_type" value="password"/>
 <input type="submit" value="Connect">
 </form>
