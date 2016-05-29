be4a40dbaada56f301c9c05cfa692a543af0867a
surjit_logiciel <surjit.singh@logicielsolutions.co.in>
2016-05-29 11:29:41 +0530

    add custom js file

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,9 @@ jQuery(function($) {
 			email:{
 				email: true
 			},
+			'additional_emails[]' : {
+				email: true
+			},
 		},
 		messages: {
 			first_name: "Please enter the first name.",
@@ -16,7 +19,7 @@ jQuery(function($) {
 
 	// default customer type 1 selected first type is commercial
 	$("input:checkbox[name='jp_customer_type1']").prop("checked", true);
-	$(this).prop("checked", true);
+	// $(this).prop("checked", true);
 	
 	$(".jobprogress-customer-type").on('change',function(){
 
@@ -43,4 +46,76 @@ jQuery(function($) {
 	  $(this).parent().removeClass('active');
 	});
 
+	//additional email
+	
+	_.templateSettings = {
+	  interpolate: /\{\{(.+?)\}\}/g
+	};
+
+	var template = _.template($('.billing-address').html());
+	$("input:checkbox[name='same_as_customer_address']").parent().parent().after(template);
+
+	// add first additional email
+	$('.start-additional-emails').on('click', function(e) {
+		if($('.additional-emails').length === 4) {
+			$('.start-additional-emails').css('pointer-events', 'none');
+		}
+		
+		var template = _.template($('.additional-email').html());
+		$('.additional-emails').last().after(template);
+
+	});
+	
+	//remove additional email
+	$('body').delegate('.additional-email-remove', 'click', function(e) {
+		$(this).parent().remove();
+		$('.start-additional-emails').css('pointer-events', 'auto');
+	});
+
+	/**
+	 * additional phones
+	 */
+	var x = 1;
+	$('body').delegate('.add-additional-phone', 'click', function(e) {
+		if($('.jobprogress-customer-phone').length === 4) {
+			$('.add-additional-phone').css('pointer-events', 'none');
+		}
+		
+		var template = _.template($('.additional-phone').html());
+
+		$('.jobprogress-customer-phone').last().after(template(
+			{
+				index: x
+			}));
+		x++;
+
+	});
+
+	$('body').delegate('.remove-additional-phone', 'click', function(e) {
+		$(this).parent().remove();
+		$('.add-additional-phone').css('pointer-events', 'auto');
+	});
+
+	$("input:checkbox[name='same_as_customer_address']").on('change', function(){
+		if( ($("input:checkbox[name='same_as_customer_address']").prop("checked")) ){
+			address = $("input:text[name='address[address]']").val();
+			city = $("input:text[name='address[city]']").val();
+			country_id = $("#address-country").select2("val");
+			state_id = $("#address-state").select2("val");
+			zip = $("input:text[name='address[zip]']").val();
+			// console.log(country_id);
+			// console.log(  $('#address-country option:eq(1)').val()  );
+			
+			$('#billing-country').select2().select2('val', $('#address-country option:eq(1)').val());
+
+			$("input:text[name='billing[address]']").val(address);
+			$("input:text[name='billing[city]']").val(city);
+			$("input:checkbox[name='billing[state_id]']").val();
+			// $("#billing-country").select2('val', country_id);
+			$("#billing-state").select2().select2('val', state_id);
+			$("input:text[name='billing[zip]']").val(zip);
+
+		}
+	});
+	
 });
