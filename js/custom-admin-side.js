a1c7cb7467d8866732dca07da2fe2aad390e48fd
surjit_logiciel <surjit.singh@logicielsolutions.co.in>
2016-05-31 18:18:30 +0530

    add  jquery.min.js file

---
+++ js/custom-admin-side.js
@@ -0,0 +1,23 @@
+jQuery(function($) {
+    
+    $('.jp-disconnect').click(function(){
+        $( "#disconnect-dialog-confirm" ).dialog('open');
+    });
+
+    $('#disconnect-dialog-confirm').dialog({
+          resizable: false,
+          height:140,
+          autoOpen: false,
+          modal: true,
+          buttons: {
+            Yes: function() {
+                $( this ).dialog( "close" );
+                $('.disconnect-form').submit();
+          },
+          No: function() {
+               $( this ).dialog( "close" );
+          }
+      }
+    });
+
+});
