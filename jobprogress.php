20f29ac890a48f9be052b538a3ececbf831caaaf
surjit_logiciel <surjit.singh@logicielsolutions.co.in>
2016-06-22 17:10:47 +0530

    array_column method add

--- jobprogress.php
+++ jobprogress.php
@@ -47,3 +47,30 @@ function dd($data = array()) {
 function format_number($number = false) {
 	return "(".substr($number, 0, 3).") ".substr($number, 3, 3)."-".substr($number,6);
 }
+
+if (! function_exists('array_column')) {
+    function array_column(array $input, $columnKey, $indexKey = null) {
+        $array = array();
+        foreach ($input as $value) {
+            if ( ! isset($value[$columnKey])) {
+                trigger_error("Key \"$columnKey\" does not exist in array");
+                return false;
+            }
+            if (is_null($indexKey)) {
+                $array[] = $value[$columnKey];
+            }
+            else {
+                if ( ! isset($value[$indexKey])) {
+                    trigger_error("Key \"$indexKey\" does not exist in array");
+                    return false;
+                }
+                if ( ! is_scalar($value[$indexKey])) {
+                    trigger_error("Key \"$indexKey\" does not contain scalar value");
+                    return false;
+                }
+                $array[$value[$indexKey]] = $value[$columnKey];
+            }
+        }
+        return $array;
+    }
+}
