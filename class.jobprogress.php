7f37306a05cc4b5013a9503bf68a6eb31f9a7a3f
surjit_logiciel <surjit.singh@logicielsolutions.co.in>
2016-08-20 17:06:49 +0530

    prefix add  on customers

--- class.jobprogress.php
+++ class.jobprogress.php
@@ -206,7 +206,7 @@ class JobProgress extends JP_Request {
 		  customer_id int(10) DEFAULT NULL,
 		  PRIMARY KEY (id)
 		)";
-        $sql = "ALTER TABLE `wp_customers` ADD referred_by_id int(12) NULL, ADD referred_by_type varchar(255) DEFAULT NULL, ADD referred_by_note text DEFAULT NULL;";
+        $sql = "ALTER TABLE ".$this->wpdb->prefix."customers ADD referred_by_id int(12) NULL, ADD referred_by_type varchar(255) DEFAULT NULL, ADD referred_by_note text DEFAULT NULL;";
 		$this->wpdb->show_errors = false;
 		$this->wpdb->query($customer_query);
 		$this->wpdb->query($sql);
